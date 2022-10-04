caab4bfaa68569fa2f12c40e27db75aaf3ebadc7
Abhay Sharma <110305361+AbhaySharma-3014@users.noreply.github.com>
2022-10-04 17:46:37 +0530

    Update streamlit_host.py

--- streamlit_host.py
+++ streamlit_host.py
@@ -30,17 +30,17 @@ if uploaded_file is not None:
     # From here onwards there is another model to predict
     model1 = tf.keras.models.load_model('set2detection.h5')
     image=resized
-    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    gray = cv.GaussianBlur(gray, (5, 5), 0)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
 
     # Threshold the image, then perform a series of erosions +
     # dilations to remove any small regions of noise
-    thresh = cv.threshold(gray, 45, 255, cv.THRESH_BINARY)[1]
-    thresh = cv.erode(thresh, None, iterations=2)
-    thresh = cv.dilate(thresh, None, iterations=2)
+    thresh = cv2.threshold(gray, 45, 255, cv.THRESH_BINARY)[1]
+    thresh = cv2.erode(thresh, None, iterations=2)
+    thresh = cv2.dilate(thresh, None, iterations=2)
 
     # Find contours in thresholded image, then grab the largest one
-    cnts = cv.findContours(thresh.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    cnts = cv2.findContours(thresh.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     c = max(cnts, key=cv.contourArea)
 
@@ -53,7 +53,7 @@ if uploaded_file is not None:
     # crop new image out of the original image using the four extreme points (left, right, top, bottom)
     new_image = image[extTop[1]:extBot[1], extLeft[0]:extRight[0]]
 
-    image = cv.resize(new_image, dsize=(240, 240), interpolation=cv.INTER_CUBIC)
+    image = cv2.resize(new_image, dsize=(240, 240), interpolation=cv.INTER_CUBIC)
     image = image / 255.
 
     image = image.reshape((1, 240, 240, 3))
